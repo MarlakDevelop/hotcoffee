1432fe24abd62504d1c383290c052e631577cc24
Marlak's Computer <mik.myk2015@gmail.com>
2021-02-15 17:04:15 +0400

    Adding a new stuff

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-class MyWidget(QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
         self.con = sqlite3.connect('coffee.sqlite')
         self.load_table()
+        self.tableWidget.cellDoubleClicked.connect(self.open_film)
+        self.pushButton.clicked.connect(self.create_film)
 
     def load_table(self):
         self.tableWidget.clear()
@@ -28,9 +30,93 @@ class MyWidget(QMainWindow):
             for j, val in enumerate(elem):
                 self.tableWidget.setItem(i, j, QtWidgets.QTableWidgetItem(str(val)))
 
+    def open_film(self, row, column):
+        pk = int(self.tableWidget.item(row, 0).text())
+        coffee_form = EditCoffeeForm(self)
+        coffee_form.pk = pk
+        coffee_form.load_coffee()
+        coffee_form.show()
+
+    def create_film(self):
+        coffee_form = AddCoffeeForm(self)
+        coffee_form.show()
+
+
+class AddCoffeeForm(QMainWindow):
+    def __init__(self, root, **kwargs):
+        super().__init__(root, **kwargs)
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.root = root
+        self.pushButton.clicked.connect(self.create_coffee)
+
+    def create_coffee(self):
+        try:
+            cur = self.root.con.cursor()
+            cur.execute(f"""
+                        INSERT INTO coffee(sort_name, roasting, type, taste, price, volume)
+                        VALUES(
+                            "{self.lineEdit.text().strip()}",
+                            "{self.spinBox.value()}",
+                            "{self.lineEdit_2.text().strip()}",
+                            "{self.textEdit.toPlainText()}",
+                            "{self.spinBox_2.value()}",
+                            "{self.spinBox_3.value()}"
+                        )""")
+            self.root.con.commit()
+            self.root.load_table()
+            self.close()
+        except Exception:
+            return None
+
+
+class EditCoffeeForm(QMainWindow):
+    def __init__(self, root, **kwargs):
+        super().__init__(root, **kwargs)
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.root = root
+        self.pk = 0
+        self.pushButton.setText('Обновить')
+        self.spinBox_2.setMaximum(10000)
+        self.pushButton.clicked.connect(self.update_coffee)
+
+    def load_coffee(self):
+        try:
+            cur = self.root.con.cursor()
+            result = cur.execute(f"""
+              SELECT * FROM coffee
+              WHERE id = "{self.pk}"
+            """, ).fetchone()
+            self.lineEdit.setText(str(result[1]))
+            self.lineEdit_2.setText(str(result[3]))
+            self.spinBox.setValue(int(result[2]))
+            self.spinBox_2.setValue(int(result[5]))
+            self.spinBox_3.setValue(int(result[6]))
+            self.textEdit.setPlainText(str(result[4]))
+        except Exception:
+            self.close()
+
+    def update_coffee(self):
+        try:
+            cur = self.root.con.cursor()
+            cur.execute(f"""
+                        UPDATE coffee
+                        SET sort_name = "{self.lineEdit.text().strip()}",
+                            roasting = "{self.spinBox.value()}",
+                            type = "{self.lineEdit_2.text().strip()}",
+                            taste = "{self.textEdit.toPlainText()}",
+                            price = "{self.spinBox_2.value()}",
+                            volume = "{self.spinBox_3.value()}"
+                        WHERE id = {self.pk}
+                        """)
+            self.root.con.commit()
+            self.root.load_table()
+            self.close()
+        except Exception:
+            return None
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MyWidget()
+    ex = MainWindow()
     ex.show()
     sys.exit(app.exec_())
